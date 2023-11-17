8c774fbad95d65ea58a6036fb7a6191dcb7ca0c6
shreya1569AS <bshreya932@gmail.com>
2023-11-17 16:09:16 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'font-poppins': '"Poppins"',
+        'roboto': '"Roboto"'
+      }
+    },
+  },
+  plugins: [],
+}
+
